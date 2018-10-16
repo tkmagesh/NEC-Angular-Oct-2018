054fb6a8bc34305e4ed180edbc494b0288c6dd2c
Magesh <tkmagesh77@gmail.com>
2018-10-16 10:53:19 +0530

    calculator-2 solution - part - 1

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -4,17 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { GreeterComponent } from './greeter/greeter.component';
 import { CalculatorComponent } from './calculator/calculator.component';
-
+import { CalculatorTwoComponent } from './calculator/calculatorTwo.component';
 @NgModule({
   declarations: [
     AppComponent
     , GreeterComponent
     , CalculatorComponent
+    , CalculatorTwoComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent, GreeterComponent, CalculatorComponent]
+  bootstrap: [AppComponent, GreeterComponent, CalculatorComponent, CalculatorTwoComponent]
 })
 export class AppModule { }
--- first-app/src/app/calculator/calculator.component.ts
+++ first-app/src/app/calculator/calculator.component.ts
@@ -1,25 +1,12 @@
 import { Component } from '@angular/core';
+import { CalculatorModel } from './CalculatorModel';
 
 @Component({
 	selector : 'app-calculator',
 	templateUrl : 'calculator.component.html'
 })
 export class CalculatorComponent{
-	n1 : number = 0;
-	n2 : number = 0;
+	
+	model : CalculatorModel = new CalculatorModel();
 
-	result : number = 0;
-
-	onAddClick(){
-		this.result = this.n1 + this.n2;
-	}
-	onSubtractClick(){
-		this.result = this.n1 - this.n2;
-	}
-	onMultiplyClick(){
-		this.result = this.n1 * this.n2;
-	}
-	onDivideClick(){
-		this.result = this.n1 / this.n2;
-	}
 }
