6f6378ae2b3b6b61aad8cee14a45dd526e413153
Magesh <tkmagesh77@gmail.com>
2018-10-16 15:43:35 +0530

    use cases 1 to 5 implementation

---
+++ bugTrackerApp/src/app/bugTracker/bugTracker.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+import { Bug } from './models/Bug';
+
+@Component({
+	selector : 'app-bug-tracker',
+	templateUrl : 'bugTracker.component.html'
+})
+export class BugTrackerComponent{
+	bugs  : Bug[] = [];
+
+	onAddNewClick(bugName : string){
+		let newBug = {
+			name : bugName,
+			isClosed : false
+		};
+		this.bugs.push(newBug);
+	}
+
+	onBugNameClick(bugToToggle : Bug){
+		bugToToggle.isClosed = !bugToToggle.isClosed;
+	}
+
+	onRemoveClosedClick(){
+		this.bugs = this.bugs.filter(bug => !bug.isClosed);
+	}
+
+	getClosedCount(){
+		return this.bugs.reduce((result, bug) => bug.isClosed ? ++result : result, 0);
+	}
+}
